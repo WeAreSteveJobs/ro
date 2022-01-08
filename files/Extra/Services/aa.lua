046c15b9c8b895e41bf665ce234952e919a9613a
WeAreSteveJobs <97238796+WeAreSteveJobs@users.noreply.github.com>
2022-01-08 16:56:15 -0300

    Services

---
+++ files/Extra/Services/aa.lua
@@ -0,0 +1,18 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Warning ", "BloodTheme")
+
+local Tab = Window:NewTab("WARNING")
+local Section = Tab:NewSection("    ")
+
+Section:NewLabel("this hub has been discontinued")
+
+Section:NewLabel("want to run")
+
+Section:NewButton("Yes", "Execute Mod menu", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WeAreSteveJobs/ro/main/cry.lua"))()
+end)
+
+Section:NewButton("No", "Close Gui and Kicks from Server", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WeAreSteveJobs/fddfffdfdsfdsffdsf/main/load/kills/super/dont/agmgry/sadboi/dff/dd/psu/and%20asad/folder/modelMain/TX"))()
+end)
+
